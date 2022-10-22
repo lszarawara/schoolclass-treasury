397382f576d111bc9e698f8bd3bddca5e2847b64
Patryk <patryk.marcisz.v2@gmail.com>
2022-10-22 11:06:33 +0200

    h2-starter

---
+++ src/main/resources/data.sql
@@ -0,0 +1,24 @@
+INSERT INTO `user` VALUES
+    (1000,'lukasz.szarawara@gmail.com','Lukasz','Admin','admin','admin','ROLE_ADMIN'),
+    (1001,'kopko','Jan','Kopko','kopko','kopko','ROLE_USER'),
+    (1002,'koszko@wp.pl','Janusz','Koszko','koszko','koszko','ROLE_USER'),
+    (1003,'polak@wp.pl','Janusz','Polak','polak','polak','ROLE_ADMIN');
+
+INSERT INTO `school_class` VALUES (1,'SP261 1a'),
+                                  (2,'SP261 1b'),
+                                  (3,'SP261 1c'),
+                                  (4,'SP261 1d'),
+                                  (5,'SP261 1e'),
+                                  (6,'SP261 2a'),
+                                  (8,'LOXX 1a'),
+                                  (9,'LOXX 1b'),
+                                  (10,'LOXX 1c');
+
+INSERT INTO `child` VALUES (1000,'Janusz','Antyczny',1),
+                           (1001,'Barbara','Batyczna',1),
+                           (1002,'Jurek','Maszko',1),
+                           (1003,'Edek','Borowik',2),
+                           (1004,'Krysia','Bogacka',2),
+                           (1005,'Antoś','Kotas',3);
+
+
